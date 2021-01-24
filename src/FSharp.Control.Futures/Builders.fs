ef51dc574f801c4fd9df3c6ae0bce4f04ae8ffa1
Vladislav Podporkin <vavus44375@gmail.com>
2021-01-24 04:37:37 +0300

    Add RawStateMachineFutureBuilder

--- src/FSharp.Control.Futures/Builders.fs
+++ src/FSharp.Control.Futures/Builders.fs
@@ -46,9 +46,7 @@ module FutureBuilderImpl =
 
 
 
-// -------------------
-// SrtpFutureBuilder
-// -------------------
+
 
 [<AutoOpen>]
 module Helpers =
@@ -57,56 +55,76 @@ module Helpers =
         | ValueChoice1Of2 of item1: 'a
         | ValueChoice2Of2 of item2: 'b
 
+module StateMachines =
 
-type IStateMachine<'a> =
-    abstract member MoveNext: waker: Waker -> Poll<'a>
+    type IStateMachine<'a> =
+        abstract member MoveNext: waker: Waker -> Poll<'a>
 
-[<Struct; NoComparison; NoEquality>]
-type ReadyStateMachine<'a> =
-    { Value: 'a }
-    interface IStateMachine<'a> with member this.MoveNext(_) = Ready this.Value
+    [<Struct; NoComparison; NoEquality>]
+    type ReadyStateMachine<'a> =
+        { Value: 'a }
+        interface IStateMachine<'a> with member this.MoveNext(_) = Ready this.Value
 
-[<Struct; NoComparison; NoEquality>]
-type LazyStateMachine<'a> =
-    { mutable State: ValueChoice<unit -> 'a, 'a> }
-    interface IStateMachine<'a> with
-        member this.MoveNext(_) =
-            match this.State with
-            | ValueChoice1Of2 f ->
-                let value = f ()
-                this.State <- ValueChoice2Of2 value
-                Ready value
-            | ValueChoice2Of2 value -> Ready value
+    [<Struct; NoComparison; NoEquality>]
+    type LazyStateMachine<'a> =
+        { mutable State: ValueChoice<unit -> 'a, 'a> }
+        interface IStateMachine<'a> with
+            member this.MoveNext(_) =
+                match this.State with
+                | ValueChoice1Of2 f ->
+                    let value = f ()
+                    this.State <- ValueChoice2Of2 value
+                    Ready value
+                | ValueChoice2Of2 value -> Ready value
 
-[<Struct; NoComparison; NoEquality>]
-type FutureStateMachine<'a> =
-    { Future: Future<'a> }
-    interface IStateMachine<'a> with member this.MoveNext(waker) = Future.poll waker this.Future
+    [<Struct; NoComparison; NoEquality>]
+    type FutureStateMachine<'a> =
+        { Future: Future<'a> }
+        interface IStateMachine<'a> with member this.MoveNext(waker) = Future.poll waker this.Future
 
-[<Struct; NoComparison; NoEquality>]
-type BindStateMachine<'a, 'b, 'fa, 'fb when
-    'fa :> IStateMachine<'a> and 'fa : struct and
-    'fb :> IStateMachine<'b> and 'fb : struct >
-    =
-    { FutureA: 'fa
-      mutable FutureB: 'fb voption
-      Binder: 'a -> 'fb }
-    interface IStateMachine<'b> with
-        member this.MoveNext(waker) =
-            match this.FutureB with
-            | ValueNone ->
-                let mutable fa = this.FutureA
-                let pa = fa.MoveNext(waker)
-                match pa with
-                | Ready va ->
-                    let mutable fb = this.Binder va
-                    this.FutureB <- ValueSome fb
+    [<Struct; NoComparison; NoEquality>]
+    type ConditionalStateMachine<'a, 'ft, 'ff
+        when 'ft :> IStateMachine<'a> and 'ft : struct
+        and 'ff :> IStateMachine<'a> and 'ff : struct >
+        =
+        { Condition: bool
+          IfTrue: 'ft
+          IfFalse: 'ff }
+        interface IStateMachine<'a> with
+            member this.MoveNext(waker) =
+                if this.Condition
+                then this.IfTrue.MoveNext(waker)
+                else this.IfFalse.MoveNext(waker)
+
+    [<Struct; NoComparison; NoEquality>]
+    type BindStateMachine<'a, 'b, 'fa, 'fb when
+        'fa :> IStateMachine<'a> and 'fa : struct and
+        'fb :> IStateMachine<'b> and 'fb : struct >
+        =
+        { FutureA: 'fa
+          mutable FutureB: 'fb voption
+          Binder: 'a -> 'fb }
+        interface IStateMachine<'b> with
+            member this.MoveNext(waker) =
+                match this.FutureB with
+                | ValueNone ->
+                    let mutable fa = this.FutureA
+                    let pa = fa.MoveNext(waker)
+                    match pa with
+                    | Ready va ->
+                        let mutable fb = this.Binder va
+                        this.FutureB <- ValueSome fb
+                        fb.MoveNext(waker)
+                    | Pending -> Pending
+                | ValueSome fb ->
+                    let mutable fb = fb
                     fb.MoveNext(waker)
-                | Pending -> Pending
-            | ValueSome fb ->
-                let mutable fb = fb
-                fb.MoveNext(waker)
 
+open StateMachines
+
+// -------------------------
+// StateMachineFutureBuilder
+// -------------------------
 
 type StateMachineFutureBuilder() =
 
@@ -124,6 +142,8 @@ type StateMachineFutureBuilder() =
           BindStateMachine.FutureB = ValueNone
           BindStateMachine.Binder = f }
 
+    member inline _.ReturnFrom(sm: #IStateMachine<'a>) = sm
+
     member inline _.ReturnFrom(x: Future<'a>) = { FutureStateMachine.Future = x }
 
     member inline this.Combine(u: 'fu, x: 'fa) =
@@ -140,8 +160,51 @@ type StateMachineFutureBuilder() =
             sm.MoveNext(waker)
 
 
+// --------------------------
+// RawStateMachineFutureBuilder
+// --------------------------
+type RawStateMachineFutureBuilder() =
+
+    member inline _.Return(x: 'a) = { ReadyStateMachine.Value = x }
+
+    member inline this.Zero() = this.Return(())
+
+    member inline _.Bind(x: 'fa, f: 'a -> 'fb) =
+        { BindStateMachine.FutureA = x
+          BindStateMachine.FutureB = ValueNone
+          BindStateMachine.Binder = f }
+
+    member inline this.Bind(x: Future<'a>, f: 'a -> 'fb) =
+        { BindStateMachine.FutureA = { FutureStateMachine.Future = x }
+          BindStateMachine.FutureB = ValueNone
+          BindStateMachine.Binder = f }
+
+    member inline _.ReturnFrom(sm: #IStateMachine<'a>) = sm
+
+    member inline _.ReturnFrom(x: Future<'a>) = { FutureStateMachine.Future = x }
+
+    member inline this.Combine(u: 'fu, x: 'fa) =
+        this.Bind(u, fun () -> x)
+
+    member inline this.Delay(f: unit -> 'a) =
+        this.Bind(this.Zero(), f)
+
+    member inline this.Run(sm: #IStateMachine<'a>) = sm
+
 
 
 [<AutoOpen>]
 module StateMachineFutureBuilderImpl =
     let smfuture = StateMachineFutureBuilder()
+
+    let rawsmfuture = RawStateMachineFutureBuilder()
+
+    let inline futIfElse (cond: bool) (ifTrue: 'ft) (ifFalse: 'ff) =
+        { ConditionalStateMachine.Condition = cond
+          ConditionalStateMachine.IfTrue = ifTrue
+          ConditionalStateMachine.IfFalse = ifFalse }
+
+    let inline asFuture (sm: #IStateMachine<'a>) =
+        let mutable sm = sm
+        Future ^fun waker ->
+            sm.MoveNext(waker)
--- tests/FSharp.Control.Futures.Test/Program.fs
+++ tests/FSharp.Control.Futures.Test/Program.fs
@@ -9,76 +9,64 @@ open FSharp.Control.Futures.Base
 open FSharp.Control.Futures
 
 
-module Snowball =
-
-    type Tree<'a> =
-        | Leaf
-        | Node of Tree<'a> * 'a * Tree<'a>
-
-    let snowballFunction n =
-        let rec loop x i : Tree<int> =
-            if i >= n then
-                Leaf
-            else
-            let left = loop (x + i) (i + 1)
-            let right = loop (x + i + 1) (i + 1)
-            Node (left, x ,right)
-        loop 0 0
-
-
-    let snowballFuture n = future {
-        let rec loop x i : Future<Tree<int>> = future {
-            if i >= n then
-                return Leaf
-            else
-            let! left = loop (x + i) (i + 1)
-            and! right = loop (x + i + 1) (i + 1)
-            return Node (left, x ,right)
-        }
-        return! loop 0 0
-    }
-
-    let snowballFutureParallel n = future {
-        let rec loop x i : Future<Tree<int>> = future {
-            if i >= n then
-                return Leaf
-            else
-            let left = loop (x + i) (i + 1) |> Runtime.runOnPoolAsync
-            let right = loop (x + i + 1) (i + 1) |> Runtime.runOnPoolAsync
-            let! left = left
-            let! right = right
-            return Node (left, x ,right)
-        }
-        return! loop 0 0
-    }
-
-    let snowballAsyncParallel n = async {
-        let rec loop x i : Async<Tree<int>> = async {
-            if i >= n then
-                return Leaf
-            else
-            let left = loop (x + i) (i + 1)
-            let right = loop (x + i + 1) (i + 1)
-            let! [|left; right|] = Async.Parallel [left; right]
-            return Node (left, x ,right)
-        }
-        return! loop 0 0
-    }
-
-    let snowballAsync n = async {
-        let rec loop x i : Async<Tree<int>> = async {
-            if i >= n then
-                return Leaf
-            else
-            let! left = loop (x + i) (i + 1)
-            let! right = loop (x + i + 1) (i + 1)
-            return Node (left, x ,right)
-        }
-        return! loop 0 0
-    }
-
-//    let snowballTask n = task {
-//        let rec loop x i : Task<Tree<int>> = task {
+//module Snowball =
+//
+//    type Tree<'a> =
+//        | Leaf
+//        | Node of Tree<'a> * 'a * Tree<'a>
+//
+//    let snowballFunction n =
+//        let rec loop x i : Tree<int> =
+//            if i >= n then
+//                Leaf
+//            else
+//            let left = loop (x + i) (i + 1)
+//            let right = loop (x + i + 1) (i + 1)
+//            Node (left, x ,right)
+//        loop 0 0
+//
+//
+//    let snowballFuture n = future {
+//        let rec loop x i : Future<Tree<int>> = future {
+//            if i >= n then
+//                return Leaf
+//            else
+//            let! left = loop (x + i) (i + 1)
+//            and! right = loop (x + i + 1) (i + 1)
+//            return Node (left, x ,right)
+//        }
+//        return! loop 0 0
+//    }
+//
+//    let snowballFutureParallel n = future {
+//        let rec loop x i : Future<Tree<int>> = future {
+//            if i >= n then
+//                return Leaf
+//            else
+//            let left = loop (x + i) (i + 1) |> Runtime.runOnPoolAsync
+//            let right = loop (x + i + 1) (i + 1) |> Runtime.runOnPoolAsync
+//            let! left = left
+//            let! right = right
+//            return Node (left, x ,right)
+//        }
+//        return! loop 0 0
+//    }
+//
+//    let snowballAsyncParallel n = async {
+//        let rec loop x i : Async<Tree<int>> = async {
+//            if i >= n then
+//                return Leaf
+//            else
+//            let left = loop (x + i) (i + 1)
+//            let right = loop (x + i + 1) (i + 1)
+//            let! [|left; right|] = Async.Parallel [left; right]
+//            return Node (left, x ,right)
+//        }
+//        return! loop 0 0
+//    }
+//
+//    let snowballAsync n = async {
+//        let rec loop x i : Async<Tree<int>> = async {
 //            if i >= n then
 //                return Leaf
 //            else
@@ -88,23 +76,96 @@ module Snowball =
 //        }
 //        return! loop 0 0
 //    }
+//
+////    let snowballTask n = task {
+////        let rec loop x i : Task<Tree<int>> = task {
+////            if i >= n then
+////                return Leaf
+////            else
+////            let! left = loop (x + i) (i + 1)
+////            let! right = loop (x + i + 1) (i + 1)
+////            return Node (left, x ,right)
+////        }
+////        return! loop 0 0
+////    }
 
+open FSharp.Control.Futures.Test
+open FSharp.Quotations
 
-let runFuture depth =
-    let f = Snowball.snowballFuture depth
-    Future.run f
+//module QuoteExpr =
+//    type FutureBuilder with
+//        member _.Quote() = ()
+//        member _.Run(x: Expr) = x
+//
+//open QuoteExpr
 
-let runFutureParallel depth =
-    let f = Snowball.snowballFutureParallel depth
-    Runtime.runSync f
+module Fib =
 
-let runAsync depth =
-    let a = Snowball.snowballAsync depth
-    Async.RunSynchronously a
+    let rec fib n =
+        if n < 0 then invalidOp "n < 0"
+        if n <= 1 then n
+        else fib (n - 1) + fib (n - 2)
 
-let runAsyncParallel depth =
-    let a = Snowball.snowballAsyncParallel depth
-    Async.RunSynchronously a
+    let rec fibAsync n = async {
+        if n < 0 then invalidOp "n < 0"
+        if n <= 1 then return n
+        else
+            let! a = fibAsync (n - 1)
+            let! b = fibAsync (n - 2)
+            return a + b
+    }
+
+    let rec fibFutureNoBuilder (n: int) : Future<int> =
+        if n < 0 then raise (InvalidOperationException "n < 0")
+        if n < 2
+        then Future.ready n
+        else
+            let mutable value = -1
+            let f1 = fibFutureNoBuilder (n - 1)
+            let f2 = fibFutureNoBuilder (n - 2)
+            Future (fun waker ->
+                match value with
+                | -1 ->
+                    match Future.poll waker f1, Future.poll waker f2 with
+                    | Ready a, Ready b -> value <- (a + b); Ready (a + b)
+                    | _ -> Pending
+                | x -> Ready x
+            )
+
+    let rec fibFuture n = future {
+        if n < 0 then invalidOp "n < 0"
+        if n <= 1 then return n
+        else
+            let! a = fibFuture (n - 1)
+            let! b = fibFuture (n - 2)
+            return a + b
+    }
+
+    let rec fibRawSMFuture n = rawsmfuture {
+        let! r =
+            futIfElse
+                (n <= 1)
+                (rawsmfuture { return n })
+                (rawsmfuture {
+                    let! a = fibRawSMFuture (n - 1) |> asFuture
+                    let! b = fibRawSMFuture (n - 2) |> asFuture
+                    return a + b
+                })
+        return r
+    }
+
+    let rec fibSMFuture n = smfuture {
+        if n < 0 then invalidOp "n < 0"
+
+        if n <= 1 then return! future { return n }
+        else
+            return! future {
+                let! a = fibSMFuture (n - 1)
+                let! b = fibSMFuture (n - 2)
+                return a + b
+            }
+    }
+
 
 //let runTask depth =
 //    let task = Snowball.snowballTask depth
@@ -114,53 +175,45 @@ let runAsyncParallel depth =
 [<EntryPoint>]
 let main argv =
 
-    printfn "Main"
-    future {
-        let! x1 = future {
-            printfn "[x1] Start"
-            do! Future.sleep 2000
-            printfn "[x1] End"
-            return 2
-        }
-        and! x2 = future {
-            printfn "[x2] Start"
-            do! Future.sleep 3000
-            printfn "[x2] End"
-            return 3
-        }
-        and! x3 = future {
-            printfn "[x3]"
-            return 1
-        }
-        printfn "Merged"
-        return x1 + x2 + x3
-    }
-    |> Runtime.runSync
-    |> printfn "R: %i"
-
-    let depth = 17
-
     let sw = Stopwatch()
+
+    let n = 25
 
     printfn "Test function..."
     sw.Start()
-    for i in 1..20 do (Snowball.snowballFunction depth) |> ignore
+    for i in 1..20 do (Fib.fib n) |> ignore
     let ms = sw.ElapsedMilliseconds
     printfn "Total %i ms\n" ms
 
     printfn "Test async..."
     sw.Start()
-    for i in 1..20 do (runAsync depth) |> ignore
+    for i in 1..20 do (Fib.fibAsync n |> Async.RunSynchronously) |> ignore
+    let ms = sw.ElapsedMilliseconds
+    printfn "Total %i ms\n" ms
+
+    printfn "Test future no builder..."
+    sw.Restart()
+    for i in 1..20 do (Fib.fibFutureNoBuilder n |> Future.run) |> ignore
     let ms = sw.ElapsedMilliseconds
     printfn "Total %i ms\n" ms
 
     printfn "Test future..."
     sw.Restart()
-    for i in 1..20 do (runFuture depth) |> ignore
+    for i in 1..20 do (Fib.fibFuture n |> Future.run) |> ignore
     let ms = sw.ElapsedMilliseconds
     printfn "Total %i ms\n" ms
 
+    printfn "Test State Machine Future..."
+    sw.Restart()
+    for i in 1..20 do (Fib.fibSMFuture n |> Future.run) |> ignore
+    let ms = sw.ElapsedMilliseconds
+    printfn "Total %i ms\n" ms
 
+    printfn "Test Raw State Machine Future..."
+    sw.Restart()
+    for i in 1..20 do (Fib.fibRawSMFuture n |> asFuture |> Future.run) |> ignore
+    let ms = sw.ElapsedMilliseconds
+    printfn "Total %i ms\n" ms
 
 
 
