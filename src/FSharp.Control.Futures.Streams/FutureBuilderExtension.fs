a4b923df42942ca81db912ca6a096c22da55499b
Prunkles <prunkles.dreemurr@gmail.com>
2021-02-22 13:00:40 +0200

    Add type annotations

--- src/FSharp.Control.Futures.Streams/FutureBuilderExtension.fs
+++ src/FSharp.Control.Futures.Streams/FutureBuilderExtension.fs
@@ -7,4 +7,3 @@ open FSharp.Control.Futures
 type FutureBuilder with
     member _.For(source: IPullStream<'a>, action: 'a -> Future<unit>): Future<unit> =
         PullStream.iterAsync action source
-A
