767af99d25060b222463bd69e572abb0085d1aeb
Prunkles <prunkles.dreemurr@gmail.com>
2021-02-22 08:56:41 +0200

    Add PullStreamBuilder base

---
+++ src/FSharp.Control.Futures.Streams/PullStreamBuilder.fs
@@ -0,0 +1,26 @@
+namespace FSharp.Control.Futures.Streams
+
+open FSharp.Control.Futures
+
+
+type PullStreamBuilder() =
+
+    member _.Yield(x) = PullStream.single x
+
+    member _.YieldFrom(x: IPullStream<'a>): IPullStream<'a> = x
+
+    member _.YieldFrom(x: Future<'a>): IPullStream<'a> = notImplemented "" // TODO: Implement
+
+    member _.Zero() = PullStream.empty ()
+
+    member _.Bind(x: IPullStream<'a>, f: 'a -> IPullStream<'a>) = PullStream.collect f x
+
+    member _.Bind(x: Future<'a>, f: 'a -> IPullStream<'a>): IPullStream<'a> = notImplemented "" // TODO: Implement
+
+    member _.Combine(s1: IPullStream<'a>, s2: IPullStream<'a>): IPullStream<'a> = PullStream.append s1 s2
+
+    member _.Delay(f: unit -> IPullStream<'a>): IPullStream<'a> = notImplemented "" // TODO: Implement
+
+
+module PullStreamBuilderImpl =
+    let pullStream = PullStreamBuilder()
--- src/FSharp.Control.Futures.Streams/Utils.fs
+++ src/FSharp.Control.Futures.Streams/Utils.fs
@@ -9,3 +9,4 @@ let inline ( ^ ) f x = f x
 //let (|IsNull|IsNotNull|) x = match x with null -> IsNull | _ -> IsNotNull x
 //let (|IsNullRef|IsNotNullRef|) x = match x with _ when obj.ReferenceEquals(x, null) -> IsNullRef | _ -> IsNotNullRef x
 
+let notImplemented fmt = Printf.kprintf (System.NotImplementedException >> raise) fmt
