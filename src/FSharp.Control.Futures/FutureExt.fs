eab678645c6cfce19ade434d6d97e694eca440dd
Vladislav Podporkin <vavus44375@gmail.com>
2021-03-22 02:34:24 +0300

    Merge SystemExt.fs into FutureExt.fs

--- src/FSharp.Control.Futures/FutureExt.fs
+++ src/FSharp.Control.Futures/FutureExt.fs
@@ -6,7 +6,9 @@
 [<AutoOpen>]
 module FSharp.Control.Futures.FutureExt
 
+open System
 open System.Collections.Generic
+open System.Threading
 
 
 exception FutureCancelledException
@@ -27,6 +29,62 @@ module Future =
             | ValueSome r -> Poll.Ready r
             | ValueNone -> Poll.Pending
         <| fun () -> f.Cancel()
+
+    let sleep (dueTime: TimeSpan) =
+
+        let mutable timer: Timer = Unchecked.defaultof<_>
+        // timer ready
+        let mutable timeOut = false
+
+        let inline onWake (context: Context) _ =
+            let timer' = timer
+            timer <- Unchecked.defaultof<_>
+            timeOut <- true
+            context.Wake()
+            timer'.Dispose()
+
+        let inline createTimer context =
+            new Timer(onWake context, null, dueTime, Timeout.InfiniteTimeSpan)
+
+        Future.Core.create
+        <| fun context ->
+            if timeOut then Poll.Ready ()
+            else
+                timer <- createTimer context
+                Poll.Pending
+        <| fun () ->
+            timer.Dispose()
+            // TODO: impl
+            do ()
+
+    let sleepMs (milliseconds: int) =
+        let dueTime = TimeSpan.FromMilliseconds((float) milliseconds)
+        sleep dueTime
+
+
+    /// The simplest implementation of the Future scheduler.
+    /// Spawn a Future on current thread and synchronously waits for its Ready
+    ///
+    /// Equivalent to `(Scheduler.spawnOn anyScheduler).Join()`,
+    /// but without the cost of complex general purpose scheduler synchronization
+    let runSync (f: Future<'a>) : 'a =
+        // The simplest implementation of the Future scheduler.
+        // Based on a polling cycle (polling -> waiting for awakening -> awakening -> polling)
+        // until the point with the result is reached
+
+        use wh = new EventWaitHandle(false, EventResetMode.AutoReset)
+        let ctx =
+            { new Context() with member _.Wake() = wh.Set() |> ignore }
+
+
+        let rec wait (current: Poll<'a>) =
+            match current with
+            | Poll.Ready x -> x
+            | Poll.Pending ->
+                wh.WaitOne() |> ignore
+                wait (Future.Core.poll ctx f)
+
+        wait (Future.Core.poll ctx f)
 
     [<RequireQualifiedAccess>]
     module Seq =
