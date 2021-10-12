2f3a98a620309b5194b20675dca6c36c56de44b7
Prunkles <prunkles.dreemurr@gmail.com>
2021-10-12 18:52:52 +0300

    feat: Add Future.fuse

--- src/FSharp.Control.Futures/Extensions.fs
+++ src/FSharp.Control.Futures/Extensions.fs
@@ -9,6 +9,11 @@ open FSharp.Control.Futures.Core
 open FSharp.Control.Futures.Internals
 
 
+type FutureFuseException(message: string) = inherit Exception(message)
+type FutureFuseReadyException() = inherit FutureFuseException("Future was polled after returning Ready.")
+type FutureFuseCancelledException() = inherit FutureFuseException("Future was polled after being cancelled.")
+type FutureFuseTransitedException() = inherit FutureFuseException("Future was polled after returning Transit.")
+
 [<RequireQualifiedAccess>]
 module Future =
 
@@ -57,6 +62,37 @@ module Future =
     let sleepMs (millisecondDuration: int) =
         let duration = TimeSpan(days=0, hours=0, minutes=0, seconds=0, milliseconds=millisecondDuration)
         sleep duration
+
+    type FuseFuture<'a>(fut: Future<'a>) =
+        let mutable isReady = false
+        let mutable isCancelled = false
+        let mutable isTransited = false
+        interface Future<'a> with
+            member _.Poll(ctx) =
+                if isReady then raise (FutureFuseReadyException())
+                elif isCancelled then raise (FutureFuseCancelledException())
+                elif isTransited then raise (FutureFuseTransitedException())
+                else
+                    let p = Future.poll ctx fut
+                    match p with
+                    | Poll.Pending -> Poll.Pending
+                    | Poll.Ready x ->
+                        isReady <- true
+                        Poll.Ready x
+                    | Poll.Transit f ->
+                        isTransited <- true
+                        Poll.Transit f
+            member _.Cancel() =
+                isCancelled <- true
+
+    /// <summary>
+    /// Creates a Future that will throw a specific <see cref="FutureFuseException">FutureFuseException</see> if polled after returning Ready or Transit or being cancelled.
+    /// </summary>
+    /// <exception cref="FutureFuseReadyException">Throws if Future was polled after returning Ready.</exception>
+    /// <exception cref="FutureFuseTransitedException">Throws if Future was polled after returning Transit.</exception>
+    /// <exception cref="FutureFuseCancelledException">Throws if Future was polled after being cancelled.</exception>
+    let fuse (fut: Future<'a>) : Future<'a> =
+        upcast FuseFuture<'a>(fut)
 
     /// <summary> Creates a Computation that ignore result of the passed Computation </summary>
     /// <returns> Computation that ignore result of the passed Computation </returns>
---
+++ tests/FSharp.Control.Futures.Tests/FuseTests.fs
@@ -0,0 +1,57 @@
+module FSharp.Control.Futures.Tests.FuseTests
+
+open Expecto
+
+open FSharp.Control.Futures
+open FSharp.Control.Futures.Core
+
+// TODO: Add messages
+
+let fuseThrowsReady = test "Future.fuse throws FutureFuseReadyException if polled after returning Ready" {
+    let sourceFut = Future.ready 1
+    let fusedFut = Future.fuse sourceFut
+
+    let firstPoll = Future.poll mockContext fusedFut
+    Expect.equal firstPoll (Poll.Ready 1) ""
+
+    Expect.throwsT<FutureFuseReadyException>
+        (fun () -> Future.poll mockContext fusedFut |> ignore)
+        ""
+}
+
+let fuseThrowsTransited = test "Future.fuse throws FutureFuseTransitedException if polled after returning Transit" {
+    let transitingFut = Future.ready 1
+    let sourceFut =
+        Future.create
+        <| fun _ctx -> Poll.Transit transitingFut
+        <| fun () -> ()
+
+    let fusedFut = Future.fuse sourceFut
+
+    let firstPoll = Future.poll mockContext fusedFut
+    Expect.equal firstPoll (Poll.Transit transitingFut) ""
+
+    Expect.throwsT<FutureFuseTransitedException>
+        (fun () -> Future.poll mockContext fusedFut |> ignore)
+        ""
+}
+
+// TODO
+// let fuseThrowsCancelled = test "Future.fuse throws FutureFuseCancelledException if polled after being cancelled" {
+//     let sourceFut = Future.ready 1
+//     let fusedFut = Future.fuse sourceFut
+//
+//     let firstPoll = Future.poll mockContext fusedFut
+//     Expect.equal firstPoll (Poll.Ready 1) ""
+//
+//     Expect.throwsT<FutureFuseReadyException>
+//         (fun () -> Future.poll mockContext fusedFut |> ignore)
+//         ""
+// }
+
+[<Tests>]
+let tests =
+    testList "Future.fuse" [
+        fuseThrowsReady
+        fuseThrowsTransited
+    ]
