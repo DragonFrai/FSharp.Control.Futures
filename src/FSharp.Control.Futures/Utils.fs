295323c850187d78d0149c6c69d57e80b30868e9
Vladislav <vavus44375@gmail.com>
2021-12-10 04:12:36 +0300

    Add remove to IntrusiveList

--- src/FSharp.Control.Futures/Utils.fs
+++ src/FSharp.Control.Futures/Utils.fs
@@ -7,6 +7,8 @@ let inline internal ( ^ ) f x = f x
 let inline nullObj<'a when 'a : not struct> = Unchecked.defaultof<'a>
 let inline isNull<'a when 'a : not struct> (x: 'a) = obj.ReferenceEquals(x, null)
 let inline isNotNull<'a when 'a : not struct> (x: 'a) = not (isNull x)
+let inline internal ( =&= ) (a: 'a) (b: 'a) = obj.ReferenceEquals(a, b)
+let inline internal ( <&> ) (a: 'a) (b: 'a) = not (obj.ReferenceEquals(a, b))
 
 // let (|IsNull|IsNotNull|) x = match x with null -> IsNull | _ -> IsNotNull x
 // let (|IsNullRef|IsNotNullRef|) x = match x with _ when obj.ReferenceEquals(x, null) -> IsNullRef | _ -> IsNotNullRef x
@@ -33,7 +35,7 @@ type IntrusiveNode<'a> when 'a :> IIntrusiveNode<'a> =
 
 /// Односвязный список, элементы которого являются его же узлами.
 /// Может быть полезен для исключения дополнительных аллокаций услов на бодобии услов LinkedList.
-/// Например, список ожидающих Context или ожидающих значение 'w: IAsyncComputation
+/// Например, список ожидающих Context или ожидающих значение 'w: Future
 [<Struct>]
 type IntrusiveList<'a> when 'a :> IIntrusiveNode<'a> and 'a : not struct =
     val mutable internal startNode: 'a
@@ -44,11 +46,14 @@ module IntrusiveList =
     let create () = IntrusiveList(Unchecked.defaultof<'a>)
     let single x = IntrusiveList(x)
 
-    let isEmpty (list: IntrusiveList<'a>) =
+    let inline isEmpty (list: inref<IntrusiveList<'a>>) =
         list.startNode = null || list.endNode = null
 
+    let inline isSingle (list: inref<IntrusiveList<'a>>) =
+        list.startNode <&> null && list.startNode =&= list.endNode
+
     let pushBack (x: 'a) (list: byref<IntrusiveList<'a>>) =
-        if isEmpty list then
+        if isEmpty &list then
             list.startNode <- x
             list.endNode <- x
             x.Next <- null
@@ -57,7 +62,7 @@ module IntrusiveList =
             list.endNode <- x
 
     let popFront (list: byref<IntrusiveList<'a>>) =
-        if isEmpty list
+        if isEmpty &list
         then null
         elif list.endNode = list.startNode then
             let r = list.startNode
@@ -70,10 +75,33 @@ module IntrusiveList =
             list.startNode <- second
             first
 
-    let toList (list: byref<IntrusiveList<'a>>) : 'a list =
+    /// <summary> Delete element from list and return true on success </summary>
+    let remove (toRemove: 'a) (list: byref<IntrusiveList<'a>>): bool =
+        let mutable list = &list
+        if isEmpty &list then
+            false
+        elif list.startNode =&= toRemove then
+            list.startNode <- null
+            list.endNode <- null
+            true
+        elif list.startNode.Next <&> null then
+            let rec findParent (child: 'a) (first: 'a) (second: 'a) =
+                if child =&= second then first
+                elif second.Next =&= null then findParent child second second.Next
+                else null
+            let parent = findParent toRemove list.startNode list.startNode.Next
+            match parent with
+            | null -> false
+            | parent ->
+                parent.Next <- parent.Next.Next
+                true
+        else
+            false
+
+    let toList (list: inref<IntrusiveList<'a>>) : 'a list =
         let root = list.startNode
         let rec collect (c: 'a list) (node: 'a) =
-            if node = null then c
+            if node =&= null then c
             else collect (c @ [node]) node.Next
         collect [] root
 
