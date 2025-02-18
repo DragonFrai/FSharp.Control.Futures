bbd2b25ee4f91d8268244d35c14b991d64575353
Vladislav Podporkin <vavus44375@gmail.com>
2025-02-18 23:28:19 +0300

    refactor: Move OneShot and Mailbox to Channels sub-namespace

---
+++ src/FSharp.Control.Futures/Sync/Channels/Channel.fs
@@ -0,0 +1,15 @@
+namespace FSharp.Control.Futures.Sync.Channels
+
+open FSharp.Control.Futures
+
+
+// [<Interface>]
+// type IChannelWriter<'a> =
+//     abstract Write: msg: 'a -> unit
+//     abstract TryWrite: msg: 'a -> Result<unit, unit>
+//
+// [<Interface>]
+// type IChannelReader<'a> =
+//     abstract Read: unit -> IFuture<'a>
+//     abstract TryRead: unit -> 'a option
+
