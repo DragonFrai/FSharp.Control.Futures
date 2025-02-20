d6703d58e8d9fab10c6b74ed67395859c58aaa71
Vladislav Podporkin <vavus44375@gmail.com>
2025-02-20 08:56:16 +0300

    refactor(Futures.IO): Add stream reading extensions

--- src/FSharp.Control.Futures/IO.fs
+++ src/FSharp.Control.Futures/IO.fs
@@ -5,18 +5,43 @@ open System.IO
 open FSharp.Control.Futures
 open FSharp.Control.Futures.Transforms.FutureApmTransforms
 
+[<AutoOpen>]
+module StreamExtensions =
+
+    type Stream with
+
+        member this.FutureRead(buffer: byte array, offset: int, count: int): Future<int> =
+            let beginMethod ac s = this.BeginRead(buffer, offset, count, ac, s)
+            let endMethod ar = this.EndRead(ar)
+            Future.ofBeginEnd beginMethod endMethod
+
+        member this.FutureWrite(buffer: byte array, offset: int, count: int): Future<unit> =
+            let beginMethod ac s = this.BeginWrite(buffer, offset, count, ac, s)
+            let endMethod ar = this.EndWrite(ar)
+            Future.ofBeginEnd beginMethod endMethod
+
+        member this.FutureReadByte(): Future<int> =
+            let buffer = [| 0uy |]
+            this.FutureRead(buffer, 0, 1)
+            |> Future.map (fun c -> if c <> 0 then int buffer[0] else -1)
+
+        member this.FutureWriteByte(value: byte): Future<unit> =
+            this.FutureWrite([| value |], 0, 1)
+
+
 [<RequireQualifiedAccess>]
 module Future =
 
     module Stream =
 
-        let read (buffer: byte array) (offset: int) (count: int) (stream: Stream) : Future<int> =
-            let beginMethod ac s = stream.BeginRead(buffer, offset, count, ac, s)
-            let endMethod ar = stream.EndRead(ar)
-            Future.ofBeginEnd beginMethod endMethod
+        let inline read (buffer: byte array) (offset: int) (count: int) (stream: Stream) : Future<int> =
+            stream.FutureRead(buffer, offset, count)
 
-        let write (buffer: byte array) (offset: int) (count: int) (stream: Stream) : Future<unit> =
-            let beginMethod ac s = stream.BeginWrite(buffer, offset, count, ac, s)
-            let endMethod ar = stream.EndWrite(ar)
-            Future.ofBeginEnd beginMethod endMethod
+        let inline write (buffer: byte array) (offset: int) (count: int) (stream: Stream) : Future<unit> =
+            stream.FutureWrite(buffer, offset, count)
 
+        let inline readByte (stream: Stream) : Future<int> =
+            stream.FutureReadByte()
+
+        let inline writeByte (value: byte) (stream: Stream) : Future<unit> =
+            stream.FutureWriteByte(value)
